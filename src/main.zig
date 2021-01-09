c451e888a98ac0dfcee2c85bc9ecb57f52f5b3d8
Nameless <truemedian@gmail.com>
2021-01-09 17:20:01 -0600

    expose hzzp.Headers as part of the library

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 
+const hzzp = @import("hzzp");
+
 pub usingnamespace @import("connection.zig");
 pub usingnamespace @import("request.zig");
+
+pub const Headers = hzzp.Headers;
 
 comptime {
     std.testing.refAllDecls(@This());
