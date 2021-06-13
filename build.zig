162aae8c2bd108dd456b0c545cff3cdeb851fcf7
Nameless <truemedian@gmail.com>
2021-06-13 17:47:41 -0500

    temporary workaround for build.zig changes

--- build.zig
+++ build.zig
@@ -2,18 +2,27 @@ const std = @import("std");
 
 const Builder = std.build.Builder;
 
-pub fn build(b: *Builder) void {
-    const packages = @import("deps.zig");
+const use_submodules = true;
 
+pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
     const lib_tests = b.addTest("src/main.zig");
     lib_tests.setBuildMode(mode);
 
-    if (@hasDecl(packages, "addAllTo")) { // zigmod
-        packages.addAllTo(lib_tests);
-    } else if (@hasDecl(packages, "pkgs") and @hasDecl(packages.pkgs, "addAllTo")) { // gyro
-        packages.pkgs.addAllTo(lib_tests);
+    if (use_submodules) {
+        const package = getPackage(b, ".");
+
+        for (package.dependencies.?) |dep| {
+            lib_tests.addPackage(dep);
+        }
+    } else {
+        const packages = @import("deps.zig");
+        if (@hasDecl(packages, "addAllTo")) { // zigmod
+            packages.addAllTo(lib_tests);
+        } else if (@hasDecl(packages, "pkgs") and @hasDecl(packages.pkgs, "addAllTo")) { // gyro
+            packages.pkgs.addAllTo(lib_tests);
+        }
     }
 
     const tests = b.step("test", "Run all library tests");
@@ -23,14 +32,14 @@ pub fn build(b: *Builder) void {
 pub fn getPackage(b: *Builder, comptime prefix: []const u8) std.build.Pkg {
     var dependencies = b.allocator.alloc(std.build.Pkg, 4) catch unreachable;
 
-    dependencies[0] = .{ .name = "iguanaTLS", .path = prefix ++ "/libs/iguanaTLS/src/main.zig" };
-    dependencies[1] = .{ .name = "network", .path = prefix ++ "/libs/network/network.zig" };
-    dependencies[2] = .{ .name = "uri", .path = prefix ++ "/libs/uri/uri.zig" };
-    dependencies[3] = .{ .name = "hzzp", .path = prefix ++ "/libs/hzzp/src/main.zig" };
+    dependencies[0] = .{ .name = "iguanaTLS", .path = .{ .path = prefix ++ "/libs/iguanaTLS/src/main.zig" } };
+    dependencies[1] = .{ .name = "network", .path = .{ .path = prefix ++ "/libs/network/network.zig" } };
+    dependencies[2] = .{ .name = "uri", .path = .{ .path = prefix ++ "/libs/uri/uri.zig" } };
+    dependencies[3] = .{ .name = "hzzp", .path = .{ .path = prefix ++ "/libs/hzzp/src/main.zig" } };
 
     return .{
         .name = "zfetch",
-        .path = prefix ++ "/src/main.zig",
+        .path = .{ .path = prefix ++ "/src/main.zig" },
         .dependencies = dependencies,
     };
 }
