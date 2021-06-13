bae100ecec339738851e84dd699fd351d4cb9471
Nameless <truemedian@gmail.com>
2021-06-13 17:53:08 -0500

    examples need to use submodules

--- examples/build.zig
+++ examples/build.zig
@@ -4,7 +4,7 @@ const Builder = std.build.Builder;
 
 const examples = [_][]const u8{ "get", "post", "download", "evented" };
 
-const submodules = false;
+const submodules = true;
 
 pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
@@ -38,17 +38,17 @@ pub fn build(b: *Builder) void {
 }
 
 // can't import zfetch build.zig here, it is outside of the example's package
-fn getPackage(b: *Builder, comptime prefix: []const u8) std.build.Pkg {
+pub fn getPackage(b: *Builder, comptime prefix: []const u8) std.build.Pkg {
     var dependencies = b.allocator.alloc(std.build.Pkg, 4) catch unreachable;
 
-    dependencies[0] = .{ .name = "iguanaTLS", .path = prefix ++ "/libs/iguanaTLS/src/main.zig" };
-    dependencies[1] = .{ .name = "network", .path = prefix ++ "/libs/network/network.zig" };
-    dependencies[2] = .{ .name = "uri", .path = prefix ++ "/libs/uri/uri.zig" };
-    dependencies[3] = .{ .name = "hzzp", .path = prefix ++ "/libs/hzzp/src/main.zig" };
+    dependencies[0] = .{ .name = "iguanaTLS", .path = .{ .path = prefix ++ "/libs/iguanaTLS/src/main.zig" } };
+    dependencies[1] = .{ .name = "network", .path = .{ .path = prefix ++ "/libs/network/network.zig" } };
+    dependencies[2] = .{ .name = "uri", .path = .{ .path = prefix ++ "/libs/uri/uri.zig" } };
+    dependencies[3] = .{ .name = "hzzp", .path = .{ .path = prefix ++ "/libs/hzzp/src/main.zig" } };
 
     return .{
         .name = "zfetch",
-        .path = prefix ++ "/src/main.zig",
+        .path = .{ .path = prefix ++ "/src/main.zig" },
         .dependencies = dependencies,
     };
 }
